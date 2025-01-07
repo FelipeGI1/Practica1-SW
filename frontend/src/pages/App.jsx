ba33cb586e3683fe0a0f053a866e50530c8ba23c
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2025-01-07 16:50:57 -0300

    Update homepage

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -4,6 +4,10 @@ import '../styles/App.css';
 function App() {
     return (
         <div className="App">
+            <h1 className="title">
+              <span>Reconocimiento de</span>
+              <span>veredas</span>
+            </h1>
             {/* Tu contenido aquí */}
         </div>
     );
