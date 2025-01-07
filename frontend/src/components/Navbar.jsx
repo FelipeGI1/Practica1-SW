57bf70af6360ea2c966b65b5ddeb61d23fa019a6
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2025-01-07 17:53:57 -0300

    Fix navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,12 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 import '../styles/navbar.css';
 
-function NavBar() {
+const NavBar = ({ onToggleMenu }) => {
     return (
         <nav className="navbar">
+            <button className="hamburger-btn" onClick={onToggleMenu}>
+                <FaBars />
+            </button>
             <ul className="navbar-list">
                 <li className="navbar-item"><a href="#home">Home</a></li>
                 <li className="navbar-item"><a href="#about">About</a></li>
