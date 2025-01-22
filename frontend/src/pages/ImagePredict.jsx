084ec4f5ee9b715a7443259f6d5fba23f992f473
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2025-01-22 11:41:59 -0300

    UPDATE ImagePredict page

--- frontend/src/pages/ImagePredict.jsx
+++ frontend/src/pages/ImagePredict.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import axios from "axios";
 
 const ImagePredict = () => {
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            console.log("Archivo seleccionado:", file);
+            const formData = new FormData();
+            formData.append("file", file);
+
+            try {
+                axios
+                    .post("http://127.0.0.1:5000/predict", formData, {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
+                    })
+                    .then((res) => {
+                        console.log("Respuesta del servidor:", res.data);
+                        alert(res.data.message);
+                    });
+            } catch (error) {
+                console.error("Error al realizar la petición:", error);
+            }
+        }
+    };
+
     return (
         <div className="image-page">
             <h1>Página de Imágenes</h1>
-            <p>En esta pagina se realizan predicciones de images.</p>
+            <button onClick={() => document.getElementById("file-input").click()}>
+                Realizar predicciones
+            </button>
+            <input
+                id="file-input"
+                type="file"
+                style={{ display: "none" }}
+                accept=".png, .jpg, .jpeg"
+                onChange={handleFileChange}
+            />
         </div>
     );
 };
