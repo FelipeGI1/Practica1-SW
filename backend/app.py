164cf4138ef9be4ce33ccc3076677f44d6813979
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2025-01-22 12:50:17 -0300

    Update app.py routes

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 from predict_funct import predict_image
 import os
 import shutil
 
 app = Flask(__name__)
+CORS(app)
+
 UPLOAD_FOLDER = 'uploads'
+PREDICT_FOLDER = 'runs/segment/predict'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(PREDICT_FOLDER, exist_ok=True)
 
 def clear_upload_folder():
     for filename in os.listdir(UPLOAD_FOLDER):
@@ -30,7 +35,20 @@ def predict():
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
         predict_image(file_path)
-        return jsonify({'message': 'Prediction completed', 'file_path': file_path}), 200
+        predict_image_path = os.path.join(PREDICT_FOLDER, file.filename)
+        return jsonify({
+            'message': 'Prediction completed',
+            'original_image': f'/uploads/{file.filename}',
+            'predicted_image': f'/runs/segment/predict/{file.filename}'
+        }), 200
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+@app.route('/runs/segment/predict/<filename>')
+def predicted_file(filename):
+    return send_from_directory(PREDICT_FOLDER, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
