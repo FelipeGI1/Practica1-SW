7753fc1486591502d112374864570cc3d7d9ea1e
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2025-01-23 10:41:14 -0300

    FIX info page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import NavBar from '../components/Navbar';
 import '../styles/about.css';
 
 const About = () => {
     return (
         <div className="about-page">
-            <NavBar />
             <div className="about-content">
                 <h1>About Page</h1>
                 <p>This is the about page.</p>
